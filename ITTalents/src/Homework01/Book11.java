ec77bf1a2970a48f8304bd3655e8c86cbc40e56d
unknown <nikolovblagoy@gmail.com>
2017-06-04 19:58:25 +0300

    some book exercises

---
+++ ITTalents/src/Homework01/Book11.java
@@ -0,0 +1,34 @@
+package Homework01;
+
+import java.util.Scanner;
+
+public class Book11 {
+	public static void main(String[] args) {
+		System.out.println(6 & 4);
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		
+		int output = 0;
+		//System.out.print(number % 2);
+		output = output * 10 + (number % 2);
+		number /= 2;
+		//System.out.print(number % 2);
+		output = output * 10 + (number % 2);
+		number /= 2;
+		//System.out.print(number % 2);
+		output = output * 10 + (number % 2);
+		number /= 2;
+		//System.out.print(number % 2);
+		output = output * 10 + (number % 2);
+		number /= 2;
+		//System.out.print(number % 2);
+		output = output * 10 + (number % 2);
+		number /= 2;
+		//System.out.print(number % 2);
+		output = output * 10 + (number % 2);
+		number /= 2;
+		System.out.println(output);
+		
+		System.out.println(~10);
+	}
+}
